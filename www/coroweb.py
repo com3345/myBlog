9397173c818ab1a950b396578674ff5eef3bc006
com3345 <deardoublelee@gmail.com>
2016-04-14 21:09:05 +0900

    defined request and response process

--- www/coroweb.py
+++ www/coroweb.py
@@ -2,6 +2,9 @@ import functools
 import asyncio
 import inspect
 import logging
+from urllib import parse
+from aiohttp import web
+from apis import APIError
 
 
 def get(path):
@@ -25,41 +28,178 @@ def post(path):
         return wrapper
     return decorator
 
+# >>> from inspect import signature
+# >>> def foo(a, *, b:int, **kwargs):
+# ...     pass
+
+# >>> sig = signature(foo)
+
+# >>> str(sig)
+# '(a, *, b:int, **kwargs)'
+
+# >>> str(sig.parameters['b'])
+# 'b:int'
+
+# >>> sig.parameters['b'].annotation
+# <class 'int'>
+
+# 关于inspect.Parameter 的  kind 类型有5种：
+# POSITIONAL_ONLY        只能是位置参数
+# POSITIONAL_OR_KEYWORD  可以是位置参数也可以是关键字参数
+# VAR_POSITIONAL         相当于是 *args
+# KEYWORD_ONLY           关键字参数且提供了key，相当于是 *,key
+# VAR_KEYWORD            相当于是 **kw
+
+# 如果handler函数需要传入命名关键字参数（KEYWORD_ONLY）而且参数没有默认值的话：
+# 获取这些‘没有默认值的命名关键字参数’
+
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in param.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+# 获取handler函数的命名关键字参数
+
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in param.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+
+def has_named_kw_args(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
+
+
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
+
 
 class RequestHandler(object):
     """docstring for RequestHandler"""
     def __init__(self, arg):
         self._app = app
         self._func = fn
+        # 检测handler函数的参数类型
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_name_kw_arg = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
 
     async def __call__(self, request):
         kw = None
-        r = await self._func(**kw)
-        return r
+        logging.info(' %s : has_request_arg = %s, has_var_kw_arg = %s, has_named_kw_args = %s, get_named_kw_args = %s, get_required_kw_args = %s ' % (__name__, self._has_request_arg, self._has_var_kw_arg, self._has_name_kw_arg, self._named_kw_args, self._required_kw_args))
+        # 如果handler需要传入命名关键字参数或者可变参数的话
+        if self._has_var_kw_arg or self._has_name_kw_arg or self._required_kw_args:
+            # 如果是post请求，则读请求的body
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-Type')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON Body must be object')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type:%s' % request.content_type)
 
-    def add_route(app, fn):
-        method = getattr(fn, '__method__', None)
-        path = getattr(fn, '__route__', None)
-        if path is None or method is None:
-            raise ValueError('@get or @post not defined in %s' % str(fn))
-        if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
-            fn = asyncio.coroutine(fn)
-        logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
-        app.router.add_route(method, path, RequestHandler(app, fn))
-
-    def add_routes(app, module_name):
-        n = module_name.rfind('.')
-        if n == (-1):
-            mod = __import__(module_name, globals(), locals())
+            if request.method == 'GET':
+                qs = request.query_string
+                logging.info('qs = %s' % qs)
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+            logging.info('kw = %s' % kw)
         else:
-            name = module_name[n + 1:]
-            mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
-        for attr in dir(mod):
-            if attr.startswith('_'):
-                continue
-            fn = getattr(mod, attr)
-            if callable(fn):
-                method = getattr(fn, '__method__', None)
-                path = getattr(fn, '__route__', None)
-                if method and path:
-                    add_route(app, fn)
+            if not self._has_var_kw_arg and self._named_kw_args:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+
+        if self._has_request_arg:
+            kw['request'] = request
+
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if name not in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = await self._func(**kw)
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
+
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
+
+
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n + 1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
