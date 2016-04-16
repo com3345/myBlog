a56a672e2387e94f69ffb0c3626c0298a04544c6
com3345 <deardoublelee@gmail.com>
2016-04-17 05:14:28 +0900

    comments parts

--- www/handlers.py
+++ www/handlers.py
@@ -2,6 +2,7 @@ from coroweb import get, post
 from models import User, Blog, Comment, next_id
 from aiohttp import web
 from apis import APIValueError, APIPermissionError, Page
+from markdown2 import markdown
 
 import time
 import re
@@ -77,15 +78,17 @@ async def cookie2user(cookie_str):
 
 
 @get('/')
-async def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
-    ]
+async def index(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    page = Page(num, page_index)
+    if num == 0:
+        blogs = []
+    else:
+        blogs = await Blog.findAll(orderBy='created_at desc', limit=(page.offset, page.limit))
     return {
         '__template__': 'blogs.html',
+        'page': page,
         'blogs': blogs
     }
 
@@ -180,10 +183,10 @@ async def authenticate(*, email, passwd):
 @get('/blog/{id}')
 async def get_blog(id):
     blog = await Blog.find(id)
-    comments = await Comment.findAll('blog_id=?', [id], orderBy='create_at desc')
+    comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
     for c in comments:
         c.html_content = text2html(c.content)
-    blog.html_content = markdown2.markdown(blog.content)
+    blog.html_content = markdown(blog.content)
     return {
         '__template__': 'blog.html',
         'blog': blog,
@@ -249,8 +252,80 @@ async def manage(*, page='1'):
 @post('/api/blogs/{id}/delete')
 async def api_delete_blog(request, *, id):
     check_admin(request)
-    print('check_admin correct!')
     blog = await Blog.find(id)
-    print('find blog!')
     await blog.remove()
     return dict(id=id)
+
+
+@post('/api/blogs/{id}')
+async def api_update_blog(id, request, *, name, summary, content):
+    check_admin(request)
+    blog = await Blog.find(id)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty.')
+    blog.name = name.strip()
+    blog.summary = summary.strip()
+    blog.content = content.strip()
+    await blog.update()
+    return blog
+
+
+@get('/manage/blogs/edit')
+def manage_edit_blog(*, id):
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': id,
+        'action': '/api/blogs/%s' % id
+    }
+
+
+@get('/api/comments')
+async def api_get_comments(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Comment.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, comments=())
+    comments = await Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, comments=comments)
+
+
+@get('/manage/comments')
+async def get_comments(*, page='1'):
+    return {
+        '__template__': 'manage_comments.html',
+        'page_index': get_page_index(page)
+    }
+
+
+@post('/api/comments/{id}/delete')
+async def api_delete_comments(request, *, id):
+    check_admin(request)
+    comment = await Comment.find(id)
+    await comment.remove()
+    return dict(id=id)
+
+
+@post('/api/blogs/{id}/comments')
+async def api_create_comment(id, request, *, content):
+    user = request.__user__
+    if user is None:
+        raise APIPermissionError('Please signin first.')
+    if not content or not content.strip():
+        raise APIValueError('content')
+    blog = await Blog.find(id)
+    if blog is None:
+        raise APIResourceNotFoundError('Blog')
+    comment = Comment(
+        blog_id=blog.id,
+        user_id=user.id,
+        user_name=user.name,
+        user_image=user.image,
+        content=content.strip()
+    )
+    await comment.save()
+    return comment
